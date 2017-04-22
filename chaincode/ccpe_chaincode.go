736b010c264ac99c03e4794c3f020574d4880ff1
greenish7 <greenishupwork@gmail.com>
2017-04-22 19:36:00 +0800

    trhtrhrt

--- chaincode/ccpe_chaincode.go
+++ chaincode/ccpe_chaincode.go
@@ -216,9 +216,12 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 				}
 			}
 		}
+		var key string
+		key = args[1]
+		valAsbytes, err := stub.GetState(key)
 
-		jsonAsBytes, _ := json.Marshal(founded)
-		return jsonAsBytes, nil
+		//jsonAsBytes, _ := json.Marshal(founded)
+		return valAsbytes, nil
 	} else if fun == "findLatestBySeller" {
 		if len(args) != 3 {
 			return nil, errors.New("Incorrect number of arguments. Expecting function name and name of the var to query")
